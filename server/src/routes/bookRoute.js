22200e7e741b4bb085ef1676ed493972c535b098
Om Patel <omupatel7113@gmail.com>
2024-07-14 10:01:24 +0530

    update book

--- server/src/routes/bookRoute.js
+++ server/src/routes/bookRoute.js
@@ -75,6 +75,57 @@ router.get("/books/:id", async (req, res) => {
   }
 });
 
+// UPDATE a book by ID
+router.put("/books/:id", async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    const {
+      title,
+      author,
+      genre,
+      publicationYear,
+      price,
+      quantity,
+      publisher,
+    } = req.body;
+
+    // Validate request body
+    if (
+      !title ||
+      !author ||
+      !genre ||
+      !publicationYear ||
+      !price ||
+      !quantity ||
+      !publisher
+    ) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+
+    const updatedBook = {
+      title,
+      author,
+      genre,
+      publicationYear,
+      price,
+      quantity,
+      publisher,
+    };
+
+    const book = await Book.findByIdAndUpdate(bookId, updatedBook, {
+      new: true,
+    });
+
+    if (!book) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+
+    res.status(200).json({ success: "Book Updated Successfully!", book });
+  } catch (error) {
+    res.status(500).json({ error: "Server Error..!" });
+  }
+});
+
 
 
 module.exports = router;
