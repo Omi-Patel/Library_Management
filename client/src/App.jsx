587378f1b4050b881d9aeda974bbd84874638150
Parmesh119 <parmeshb90@gmail.com>
2024-07-14 11:17:20 +0530

    forgot and verify password added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,9 @@ import Navbar from './components/Navbar'
 import Signin from './components/Signin'
 import BookGrid from './components/Bookgrid';
 import AddBook from './components/Admin/Addbook';
+import Profile from './components/Profile';
+import Forgot_Password from './components/Forgot_Password';
+import New_Password from './components/New_Password';
 
 function App() {
   return (
@@ -17,6 +20,9 @@ function App() {
         <Route path='/register' element={<SignUp />} />
         <Route path='/login' element={<Signin />} />
         <Route path='/admin/add-book' element={<AddBook />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path='/recover-password/verify-otp' element={<Forgot_Password />} />
+        <Route path='/recover-password/new-password' element={<New_Password />} />
         </Routes>
       </BrowserRouter>
     </>
